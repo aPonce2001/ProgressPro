6988aba1b8333fb94979c6799d11b859aac3da61
Andrés Ponce <andres.ponce@epn.edu.ec>
2023-08-06 21:30:43 -0500

    init: Project structure
    Se inició la estructura del proyecto basado en el modelo MVVM

--- app/src/main/java/com/msaasd/progresspro/features/launch/LaunchActivity.kt
+++ app/src/main/java/com/msaasd/progresspro/features/launch/LaunchActivity.kt
@@ -1,7 +1,8 @@
-package com.msaasd.progresspro
+package com.msaasd.progresspro.features.launch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.msaasd.progresspro.R
 
 class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
